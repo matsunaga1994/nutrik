a1ce7915c0805a5c380c73c6a138d56222893db4
matsunaga1994 <82854197+matsunaga1994@users.noreply.github.com>
2021-05-24 12:40:38 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "foods#index"
+    resources :foods, only:[:index]
 end
