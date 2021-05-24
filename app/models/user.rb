b66daed7a8c21066c61d9e80ca5225c3511a2e85
matsunaga1994 <82854197+matsunaga1994@users.noreply.github.com>
2021-05-24 22:03:37 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,28 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  with_options presence: true do
+    validates :nickname
+    validates :family_name
+    validates :first_name
+    validates :family_name_kana
+    validates :first_name_kana
+    validates :birthday
+  end
+
+
+  with_options format:{ with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}\z/i } do
+    validates :password
+  end
+
+  with_options format:{ with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :family_name
+    validates :first_name
+  end
+
+  with_options format:{ with: /\A[ァ-ヶ一]+\z/ } do
+    validates :family_name_kana
+    validates :first_name_kana
+  end
 end
