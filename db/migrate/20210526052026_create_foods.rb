38643ddf80af83f6f53ba58eaa54cecc9df2c3ba
matsunaga1994 <82854197+matsunaga1994@users.noreply.github.com>
2021-05-26 14:49:15 +0900

    foodモデルの作成

---
+++ db/migrate/20210526052026_create_foods.rb
@@ -0,0 +1,14 @@
+class CreateFoods < ActiveRecord::Migration[6.0]
+  def change
+    create_table :foods do |t|
+      t.string :title,    null: false
+      t.integer :time_id, null: false
+      t.integer :cost_id, null: false
+      t.text :comment,    null: false
+      t.text :ingredient, null: false
+      t.text :recipe,     null: false
+      t.refeences :user,  null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
