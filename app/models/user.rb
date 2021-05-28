f9069d720f39f3a0934bf82af65af67cc2d355fc
matsunaga1994 <82854197+matsunaga1994@users.noreply.github.com>
2021-05-28 18:07:56 +0900

    メソッドの追記（モデル）

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,4 +29,17 @@ class User < ApplicationRecord
   end
 
   has_many :sns_credentials
+
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+      email: auth.info.email
+    )
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    { user: user, sns: sns }
+  end
 end
