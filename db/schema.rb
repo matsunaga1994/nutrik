4d92740eab48b4c2f3cd3b87844564a9c2782330
matsunaga1994 <82854197+matsunaga1994@users.noreply.github.com>
2021-05-31 14:56:13 +0900

    ingredientsモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_28_080429) do
+ActiveRecord::Schema.define(version: 2021_05_31_054433) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -44,6 +44,15 @@ ActiveRecord::Schema.define(version: 2021_05_28_080429) do
     t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
+  create_table "ingredients", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.integer "category_id"
+    t.bigint "food_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["food_id"], name: "index_ingredients_on_food_id"
+  end
+
   create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "provider"
     t.string "uid"
@@ -73,5 +82,6 @@ ActiveRecord::Schema.define(version: 2021_05_28_080429) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "foods", "users"
+  add_foreign_key "ingredients", "foods"
   add_foreign_key "sns_credentials", "users"
 end
