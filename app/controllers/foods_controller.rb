420ee2c2a85513b1c05d15a4981bf05fc49a507d
matsunaga1994 <82854197+matsunaga1994@users.noreply.github.com>
2021-06-01 20:10:00 +0900

    コントローラーの定義

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,14 +1,25 @@
 class FoodsController < ApplicationController
   def index
+    @foods = Food.all
   end
 
   def new
-    
+    @food = FoodIngredientRecipe.new
   end
 
   def create
-    
+    @food = FoodIngredientRecipe.new(food_params)
+    if @food.valid?
+      @food.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
-  
+  private
+  def food_params
+    params.require(:food_ingredient_recipe).permit(:title, :image, :cook_time_id, :cost_id, :comment, :name, :food_group_id, :text).merge(user_id: current_user.id)
+  end
+
 end
