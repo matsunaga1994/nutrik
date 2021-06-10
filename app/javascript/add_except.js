accfc57142f9bc3f355e36f38942fb643bbc987c
matsunaga1994 <82854197+matsunaga1994@users.noreply.github.com>
2021-06-10 19:50:42 +0900

    削除イベントの実装

--- app/javascript/add_except.js
+++ app/javascript/add_except.js
@@ -1,25 +1,25 @@
 function add() {
-  const addIngredient = document.getElementById("add-ingredient-btn");
-  const addElement = document.getElementById("ingredient-main-inner");
-  const foodGroup = document.getElementById("food-group");
-  let ingredientText = document.getElementById("ingredient-main");
+  if (document.getElementById("add-ingredient-btn")) {
+    const addIngredient = document.getElementById("add-ingredient-btn");
+    const addElement = document.getElementById("ingredient-main-inner");
+    const foodGroup = document.getElementById("food-group");
+    let ingredientText = document.getElementById("ingredient-main");
+    addIngredient.addEventListener("click", function () {
+      ingredientText.insertAdjacentHTML("beforeend", addElement.outerHTML);
+    });
 
-  addIngredient.addEventListener("click", function () {
-    ingredientText.insertAdjacentHTML("beforeend", addElement.outerHTML);
-  });
+    const addRecipe = document.getElementById("add-recipe-btn");
+    const addRecipeElement = document.getElementById("food-recipe");
+    let recipeText = document.getElementById("recipe-main");
+    addRecipe.addEventListener("click", function () {
+      recipeText.insertAdjacentHTML("beforeend", addRecipeElement.outerHTML);
+    });
 
-  const addRecipe = document.getElementById("add-recipe-btn");
-  const addRecipeElement = document.getElementById("food-recipe");
-  let recipeText = document.getElementById("recipe-main");
-
-  addRecipe.addEventListener("click", function () {
-    recipeText.insertAdjacentHTML("beforeend", addRecipeElement.outerHTML);
-  });
-
-  // const remove = document.querySelectorAll(".except-btn");
-  // const parent = remove.parentNode.parentNode;
-  // remove.addEventListener('click', function(e){
-  //   console.log(e)
-  // });
+    const removeElement = document.getElementById("except-btn");
+    const parent = removeElement.parentNode.parentNode;
+    $(document).on('click', ".except-btn", function() {
+      $(this).parent().parent().remove();
+    });
+  }
 }
 window.addEventListener("load", add);
