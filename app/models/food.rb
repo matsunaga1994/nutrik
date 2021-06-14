57f37ea9f9594105faaba9b312b8c339b2634ba2
matsunaga1994 <82854197+matsunaga1994@users.noreply.github.com>
2021-06-14 16:35:58 +0900

    検索メソッド定義

--- app/models/food.rb
+++ app/models/food.rb
@@ -8,4 +8,12 @@ class Food < ApplicationRecord
   has_many :recipes, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
+
+  def self.search(search)
+    if search != ""
+      Food.where('title LIKE(?)', "%#{search}%")
+    else
+      Food.all
+    end
+  end
 end
